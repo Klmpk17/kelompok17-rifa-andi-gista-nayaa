52c2465675e69c807b197b47e1aebc5c1164d227
Kelompok17-rifa-andi-gista-naya <74819331+Klmpk17@users.noreply.github.com>
2020-12-16 19:34:05 +0700

    Update Method.php

--- Method.php
+++ Method.php
@@ -1,14 +1,12 @@
 <?php
-
-class laptop {
-
-   var $pemilik;
-   var $merk;
-   
-   
-   function hidupkan_laptop() {
-   return "Hidupkan Laptop";
-
-   function merk_laptop() {
-   return "merk asus";
+class rumah{
+  var $warna;
+  var $ukuran;
+  
+  function tampilkan_warna(){
+  }
+  function tampilkan_ukuran(){
+  }
 }
+   
+?>
